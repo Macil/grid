08eee77a19c7bf407d0c7cf9840d2a84b505a778
Chris Cowan <agentme49@gmail.com>
2025-12-04 03:11:10 -0800

    export Direction and Rotation types from main export too

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
 export * from "./grid.ts";
 export * from "./location.ts";
 export * from "./vector.ts";
+export type { Direction } from "./direction.ts";
+export type { Rotation } from "./rotation.ts";
